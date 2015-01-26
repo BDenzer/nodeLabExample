8472ceebff4d8d8c31532f7638fec679914521eb
BDenzer <denz0045@morris.umn.edu>
2015-01-26 14:37:17 -0600

    Added GPA Calculator functions- not tested.

--- public/javascript/clientJavascript.js
+++ public/javascript/clientJavascript.js
@@ -28,37 +28,7 @@ var testStringLength = function(str){
 var returnKittens = function(){
     return "kittens";
 };
-/**
- * @param number1,number2,number3 - the integers to take the average of
- * @returns (number) - returns the average of the three numbers
- */
 
 
-var returnAverage = function(number1, number2, number3){
-    return (number1+number2+number3)/3;
-};
-/**
- *
- * @param str -The letter grade to change to grade point value
- * @returns {number} - returns the grade point value
- */
-var returnGradePoint = function(str) {
-    switch (str) {
-        case "A":
-            return 4.0;
-            break;
-        case "B":
-            return 3.0;
-            break;
-        case "C":
-            return 2.0;
-            break;
-        case "D":
-            return 1.0;
-            break;
-        case "F":
-            return 0;
-            break;
-    }
-};
+
 
--- serverJavascript.js
+++ serverJavascript.js
@@ -8,11 +8,11 @@ exports.pageName = function(str){
     return str;
 };
 
-exports.returnAverage = function(number1, number2, number3){
-    return (number1+number2+number3)/3;
+exports.returnAverage = function(number1, number2, number3, totalCredits){
+    return (number1+number2+number3)/totalCredits;
 };
 
-exports.returnGradePoint = function(str){
+exports.returnGradeValue = function(str){
     switch(str) {
         case "A":
             return 4.0;
@@ -30,5 +30,16 @@ exports.returnGradePoint = function(str){
             return 0;
             break;
     }
+};
+
+exports.returnGradePoint = function(credits, letter){
+    return returnGradeValue(letter) * credits;
+};
+
+exports.returnGPA = function(grade1, grade2, grade3){
+    weightedGrade1= returnGradePoint(grade1);
+    weightedGrade2= returnGradePoint(grade2);
+    weightedGrade3= returnGradePoint(grade3);
+    return returnAverage(grade1,grade2,grade3);
 };
 
